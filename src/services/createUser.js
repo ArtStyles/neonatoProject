56a61af32fabe8bd51b9feb4c7b136d954e48866
ArtStyles <110346939+ArtStyles@users.noreply.github.com>
2023-10-16 15:33:31 -0400

    fix bug

--- src/services/createUser.js
+++ src/services/createUser.js
@@ -1,7 +1,7 @@
 import {makeFetch} from './makeFetch'
 
 
-export function createUser({username,email,password,isStaff}){
+export function CreateUser({username,email,password,isStaff}){
     const query = `mutation{
         createUser(username:"${username}",password:"${password}",email:"${email}",isStaff:${isStaff}){
           user{
